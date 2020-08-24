4abd9fb7d30be688b1e084545827a2d3aab6de24
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-23 20:37:54 -0700

    Creating FriendRequest component 🧩

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import PlayMusicSmall from './components/PlayMusicSmall';
+import FriendRequest from './components/FriendRequest';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1 className='App__title'>UI React Components ⚛</h1>
 			<PlayMusicSmall />
+			<FriendRequest />
 		</div>
 	);
 }
---
+++ src/components/FriendRequest.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './styles/friendRequest.css';
+import friendRequestImg from '../assets/images/friendRequest.png';
+import acceptIcon from '../assets/icons/accept.svg';
+import cancelIcon from '../assets/icons/cancel.svg';
+
+const FriendRequest = () => {
+	return (
+		<div className='friendRequest__container'>
+			<img
+				src={friendRequestImg}
+				alt='image-friend-request'
+				className='friendRequest__image'
+			/>
+			<div className='friendRequest__background'>
+				<div className='friendRequest__text-container'>
+					<h4>Alex Traier</h4>
+					<p>Friend request</p>
+				</div>
+				<div className='friendRequest__button-container'>
+					<button className='friendRequest__button-cancel'>
+						<img
+							src={cancelIcon}
+							alt='cancel'
+							className='friendRequest__icon'
+						/>
+					</button>
+					<button className='friendRequest__button-accept'>
+						<img
+							src={acceptIcon}
+							alt='accept'
+							className='friendRequest__icon'
+						/>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default FriendRequest;
