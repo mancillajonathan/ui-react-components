b38f201a472a31d68ce03cc4761e748763084be2
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-25 21:22:57 -0700

    Creating SmallButton component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import FriendRequest from './components/FriendRequest';
 import RestaurandCard from './components/RestaurandCard';
 import ProductCard from './components/ProductCard';
 import Weather from './components/Weather';
+import SmallButton from './components/SmallButton';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1 className='App__title'>UI React Components ⚛</h1>
+			<SmallButton />
 			<PlayMusicSmall />
 			<FriendRequest />
 			<RestaurandCard />
---
+++ src/components/SmallButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.button`
+	padding: 20px 32px;
+	background: linear-gradient(270deg, #8743ff 0%, #4136f1 100%);
+	box-shadow: 0px 15px 30px rgba(20, 102, 204, 0.16);
+	border-radius: 16px;
+	border-style: none;
+`;
+
+const Text = styled.p`
+	font-weight: 600;
+	font-size: 14px;
+	line-height: 14px;
+	color: #ffff;
+	margin: 0;
+	padding: 1px 0;
+	opacity: 1;
+`;
+
+const SmallButton = () => {
+	return (
+		<Container>
+			<Text>Button text</Text>
+		</Container>
+	);
+};
+
+export default SmallButton;
