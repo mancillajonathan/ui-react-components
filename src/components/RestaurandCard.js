a4ff2acf8b92d73edf48e20e3524532105b01d27
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-08-25 22:36:08 -0700

    Converting ReastaurantCard to Styled Components 💅

--- src/components/RestaurandCard.js
+++ src/components/RestaurandCard.js
@@ -1,30 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
 import './styles/restaurandCard.css';
 import restaurant from '../assets/images/restaurant.png';
 import chat from '../assets/icons/chat.svg';
 import heart from '../assets/icons/heart.svg';
 
+const Container = styled.div`
+	width: 342px;
+	height: 100px;
+	background-color: #ffffff;
+	border-radius: 24px;
+	box-shadow: 0px 25px 40px rgba(0, 0, 0, 0.03);
+	display: flex;
+	align-items: center;
+	box-sizing: border-box;
+	padding: 8px 29px 8px 8px;
+	position: relative;
+	margin-top: 36px;
+`;
+
+const Image = styled.img`
+	width: 84px;
+	height: 84px;
+	border-radius: 20px;
+	filter: drop-shadow(0px 20px 60px rgba(0, 0, 0, 0.15));
+`;
+
+const TextContainer = styled.div`
+	text-align: start;
+	margin-left: 16px;
+`;
+
+const ButtonContainer = styled.div`
+	position: absolute;
+	right: 20px;
+`;
+
+const Button = styled.button`
+	width: 40px;
+	height: 40px;
+	border-style: none;
+	border-radius: 12px;
+	box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.06);
+	background-color: #ffffff;
+
+	&:nth-child(2) {
+		margin-left: 14px;
+	}
+`;
+
 const RestaurandCard = () => {
 	return (
-		<div className='RestaurandCard__container'>
-			<img
-				src={restaurant}
-				alt='restaurant'
-				className='ReactaurandCard__image'
-			/>
-			<div className='RestaurandCard__text-container'>
+		<Container>
+			<Image src={restaurant} alt='restaurant' />
+			<TextContainer>
 				<h4>Papa’s Pizzeria</h4>
 				<p>3.2km</p>
-			</div>
-			<div className='RestaurandCard__button-container'>
-				<button className='RestaurandCard__button-coment'>
+			</TextContainer>
+			<ButtonContainer>
+				<Button>
 					<img src={chat} alt='chat' />
-				</button>
-				<button className='RestaurandCard__button-like'>
+				</Button>
+				<Button>
 					<img src={heart} alt='heart' />
-				</button>
-			</div>
-		</div>
+				</Button>
+			</ButtonContainer>
+		</Container>
 	);
 };
 
